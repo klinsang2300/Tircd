204a33435bc3892067b401d38a12fef620cfb81f
klinsang2300 <klinsang@gmail.com>
2025-06-06 16:46:33 +0700

    เดือน

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,39 +1,92 @@
 'use client'
-import { updateCalendar } from '@/utils/calenda'
-import cld from './css/Calendar.module.css'
 
-export default function Calenda() {
-    const data = updateCalendar();
-    return(
-       
-       <div className={cld.calendar}>
-            <div className={cld.header}> 
-               {/* <button id='prevBtn' >
-                    <i className={cld.left}></i>
-               </button> */}
-               <div  id='monthYear'>{data.monthYear}</div>
-               {/* <button id='nextBtn' >
-                    <i className={cld.right}></i>
-               </button> */}
-            </div>
-            <div className={cld.days}>
-                <div className={cld.day}>จันทร์</div>
-                <div className={cld.day}>อังคาร</div>
-                <div className={cld.day}>พุธ</div>
-                <div className={cld.day}>พฤหัส</div>
-                <div className={cld.day}>ศุกร์</div>
-                <div className={cld.day}>เสาร์</div>
-                <div className={`${cld.day} ${cld.holiday}`}>อาทิตย์</div>
-            </div>
-            <div className={cld.dates}>
-            {data.daycalendar.map((item,index)=>{
-               const holi = item.id %7===0?cld.holiday :'';
-                    return (
-                         
-                    <div  className={`${cld.date} ${item.act? cld.active:''} ${item.ina?cld.inactive:''} ${holi}`} key={index}>{item.Day}</div>)
-            })}
-            </div>
-       </div>
-      
-    )
+import cld from './css/Calendar.module.css'
+import { dayinMonth, formatMonthYear, formatYear, getMonthYear, getWeekDays } from '@/lib/date-utils'
+import { useMemo, useState } from 'react'
+
+
+
+
+export default function Calendar() {
+     const [currentDate, setCurrentDate] = useState(new Date());
+     const [mode, setMode] = useState('D');
+
+     const year = currentDate.getFullYear();
+     const month = currentDate.getMonth();
+     const weekDays = getWeekDays();
+
+     const months = useMemo(() => {
+          return getMonthYear(currentDate)
+     }, [currentDate])
+
+     const calendar = useMemo(() => {
+          return dayinMonth(year, month)
+     }, [year, month])
+
+     const formatHearder = useMemo(() => {
+          let formatM;
+          if (mode === "D") {
+               formatM = formatMonthYear(currentDate);
+          } else {
+               formatM = formatYear(currentDate)
+          }
+          return (formatM);
+     }, [currentDate, mode])
+
+     const today = new Date();
+     const isToday = (day) => {
+          const CalendarDate = new Date(day)
+          return (CalendarDate.toDateString() === new Date().toDateString());
+     };
+
+     const handleHeader = () => {
+          if (mode === "D") {
+               setMode("M")
+          }
+          else if (mode === "M") {
+               setMode("Y")
+          }
+     };
+     const handleSelectMonth = (even) => {
+          setCurrentDate(new Date(year,even.currentTarget.id,1));
+          setMode("D");
+          // setCurrentDate();
+     };
+     return (
+          <div className={cld.calendar}>
+               <div className={cld.header}>
+
+                    <div className={`${cld.monthYear}`} onClick={handleHeader} id={mode}>
+                         {formatHearder}
+                    </div>
+                    <div className={cld.header_button}>
+                         <button ><i className={cld.button_up}></i></button>
+                         <button ><i className={cld.button_down}></i></button>
+                    </div>
+               </div>
+               <div className={mode != 'D' ? cld.hide : ''}>
+                    <div className={cld.days}>
+                         {weekDays.map((item, index) => (
+                              <div key={index} className={`${cld.day} ${item.holi ? cld.holiday : ''}`}>{item.day}</div>
+                         ))}
+                    </div>
+                    <div className={cld.dates}>
+                         {calendar.day.map((item, index) => (
+                              <div className={`${cld.date} ${isToday(item.Day) ? cld.active : ''} ${item.holi ? cld.holiday : ''} ${item.ina?cld.inactive:''}`} key={index}>
+                                   {item.Day.getDate()}
+                              </div>
+                         ))}
+                    </div>
+               </div >
+               <div className={mode != 'M' ? cld.hide : ''}>
+
+                    <div className={cld.months}>
+                         {months.map((item, index) => {
+                              return (<div className={`${cld.month} ${item.act ? cld.active : ''} `} id={item.id} key={index} onClick={handleSelectMonth} >{item.month}</div>)
+                         })}
+                    </div>
+               </div>
+          </div>
+
+     )
 }
--- utils/calenda.js
+++ utils/calenda.js
@@ -1,6 +1,8 @@
-let currentDate = new Date();
+'use client'
 
-export const updateCalendar = () => {
+
+export const updateCalendar =  ( modeSelect= "D" ,currentDate = "" ) => {
+    
     const currentYear = currentDate.getFullYear();
     const currentMonth = currentDate.getMonth();
     const firstDay = new Date(currentYear, currentMonth, 0);
@@ -8,32 +10,58 @@ export const updateCalendar = () => {
     const totalDays = lastDay.getDate();
     const firstDayIndex = firstDay.getDay();
     const lastDayIndex = lastDay.getDay();
-    const monthYearString = currentDate.toLocaleString('th-TH', { month: 'long', year: 'numeric' });
+
     const day = [];
     let ind = 1;
-    for (let i = firstDayIndex; i > 0; i--) {
-        const preDay = { id: ind, Day: new Date(currentYear, currentMonth, 0 - i + 1).getDate(), act: false ,ina:true};
-        day.push(preDay);
-        ind = ind + 1;
+
+
+    let monthYearDiv = {};
+
+    if (modeSelect === "D") {
+        monthYearDiv = {
+            id: "monthYear",
+            monthYearString: currentDate.toLocaleString('th-TH', { month: 'long', year: 'numeric' })
+        }
+        for (let i = firstDayIndex; i > 0; i--) {
+            const preDay = { id: ind, Day: new Date(currentYear, currentMonth, 0 - i + 1).getDate(), act: false, ina: true };
+            day.push(preDay);
+            ind = ind + 1;
+        }
+        for (let i = 1; i <= totalDays; i++) {
+            const date = new Date(currentYear, currentMonth, i);
+
+            const preDay = { id: ind, Day: date.getDate(), act: date.toDateString() === new Date().toDateString() ? true : false, ina: false };
+            day.push(preDay);
+            ind = ind + 1;
+        }
+        for (let i = 1; i <= 7 - lastDayIndex; i++) {
+            const nextDate = new Date(currentYear, currentMonth + 1, i);
+            const nextDay = { id: ind, Day: nextDate.getDate(), act: false, ina: true }
+            day.push(nextDay)
+        }
     }
-
-    for (let i = 1; i <= totalDays; i++) {
-        const date = new Date(currentYear, currentMonth, i);
-
-        const preDay = { id: ind, Day: date.getDate(), act: date.toDateString() === new Date().toDateString() ? true : false,ina:false };
-        day.push(preDay);
-        ind = ind + 1;
-    }
-
-    for (let i = 1; i <= 7 - lastDayIndex; i++) {
-        const nextDate = new Date(currentYear, currentMonth + 1, i);
-        const nextDay = {id:ind,Day:nextDate.getDate(),act:false,ina:true}
-        day.push(nextDay)
+    else if (modeSelect === "M") {
+        monthYearDiv = {
+            id: "years",
+            monthYearString: currentDate.toLocaleString('th-TH', { year: 'numeric' })
+        }
+        for( ind =0 ;ind<12;ind++){
+            const date = new Date(currentYear,ind,1);
+            day.push({
+                index:ind,
+                month: date.toLocaleString('th-TH',{month:'long'}),
+                act: currentDate.getMonth() === date.getMonth()
+            })
+        }
     }
 
     const days = {
-        monthYear: monthYearString,
-        daycalendar : day
+        monthYear: monthYearDiv,
+        daycalendar: day
     }
-      return days;
+
+     return day;
+
 }
+
+
